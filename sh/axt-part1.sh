995253e18635b50e1d04a59c273071a521a26839
qlxi <98650473+qlxi@users.noreply.github.com>
2025-10-26 11:16:12 +0300

    Update axt-part1.sh

--- sh/axt-part1.sh
+++ sh/axt-part1.sh
@@ -14,4 +14,5 @@
 # rm -rf feeds.conf.default
 # touch feeds.conf.default
 # echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
-echo -e '\nsrc-git fancontrol https://github.com/JiaY-shi/fancontrol.git' >>feeds.conf.default
+echo -e '\nsrc-git fancontrol https://github.com/fanteral/luci-app-fancontrol.git' >>feeds.conf.default
+echo 'src-git turboacc https://github.com/chenmozhijin/turboacc.git' >>feeds.conf.default
